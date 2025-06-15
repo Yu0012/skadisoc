912f2a76670cb90bdff4944e3de7d968e6ede9c1
zhen yu <j21034337@student.newinti.edy.my>
2025-06-15 12:17:46 +0800

    run local

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const isLocalhost = window.location.hostname === "localhost";
 
 const config = {
   API_BASE: isLocalhost
-    ? "http://localhost:5000/api"
+    ? "http://localhost:5000"
     : "https://us-central1-skadisocmed-954f4.cloudfunctions.net/api"
 };
 
