3348e51a80748088af86586ed87101a839b8342a
zhen yu <j21034337@student.newinti.edy.my>
2025-06-29 13:30:45 +0800

    cleaning

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,7 +3,7 @@ const isLocalhost = window.location.hostname === "localhost";
 const config = {
   API_BASE: isLocalhost
     ? "http://localhost:5000"
-    : "https://us-central1-skadisocmed-954f4.cloudfunctions.net/api"
+    : "https://us-central1-skadisocmed-954f4.cloudfunctions.net/api" //firebase api
 };
 
 export default config;
--- functions/config.js
+++ functions/config.js
@@ -3,7 +3,7 @@ const isLocal = process.env.MODE === 'local';
 const config = {
   API_BASE: isLocal
     ? 'http://localhost:5000'
-    : 'https://us-central1-skadisocmed-954f4.cloudfunctions.net/api'
+    : 'https://us-central1-skadisocmed-954f4.cloudfunctions.net/api' //firebase api
 };
 
 module.exports = config;
